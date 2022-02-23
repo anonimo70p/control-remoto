de68ea9d6d2c4139296524e87a4a01e0eb25d74a
anonimo70p <98742584+anonimo70p@users.noreply.github.com>
2022-02-23 09:26:57 +0100

    Hemos conseguido que antes de chocar se vaya para atras

--- main.ts
+++ main.ts
@@ -5,7 +5,20 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (0 == receivedNumber) {
         maqueen.motorStop(maqueen.Motors.All)
     }
+    if (3 == receivedNumber) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 199)
+        maqueen.motorStop(maqueen.Motors.M2)
+    }
+    if (4 == receivedNumber) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 199)
+        maqueen.motorStop(maqueen.Motors.M1)
+    }
 })
+radio.setGroup(183)
 basic.forever(function () {
-    radio.setGroup(183)
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
+        maqueen.motorStop(maqueen.Motors.All)
+        basic.pause(200)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 79)
+    }
 })
