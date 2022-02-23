b9978b2f5f9f206f166c1c316cd50064df59efd8
anonimo70p <98742584+anonimo70p@users.noreply.github.com>
2022-02-23 09:04:21 +0100

    Que hemos puesTo que ande el coche

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (1 == receivedNumber) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    }
+    if (0 == receivedNumber) {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+})
 basic.forever(function () {
-	
+    radio.setGroup(183)
 })
